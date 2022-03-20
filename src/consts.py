4346a7249be21c493d58838ca4a3dd4faa7b2ead
Abdo <abd.nh25@gmail.com>
2022-03-20 05:49:38 +0300

    Formatting

--- src/consts.py
+++ src/consts.py
@@ -3,5 +3,3 @@ import os
 ADDON_NAME = "Bkrs Downloader"
 ADDON_DIR = os.path.dirname(__file__)
 ICONS_DIR = os.path.join(ADDON_DIR, "icons")
-# USER_FILES = os.path.join(ADDON_DIR, "user_files")
-# os.makedirs(USER_FILES, exist_ok=True)
